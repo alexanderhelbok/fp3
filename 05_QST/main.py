c1c5625e1b29b14baa572e60ba27355db3aa64b4
alex <alexander.helbok@hotmail.com>
2025-05-26 20:13:39 +0200

    QST fidelity

--- 05_QST/main.py
+++ 05_QST/main.py
@@ -124,72 +124,80 @@ def load_data(input, output):
         names = ["HH", "VH", "HV", "VV",]
         angles_hwp = [[None, None], [40, None], [None, 43], [40, 43]]
         angels_qwp = [[None, None], [None, None], [None, None], [None, None]]
-        counts = [134, 12945, 11068, 599]
+        counts = [161, 8555, 13752, 1105]
         append_to_output(output, names, counts)
 
         #HD
         names = ["HD", "VD", "HA", "VA",]
         angles_hwp = [[None, 20], [40, 20], [None, 65.5], [40, 65.5]]
         angels_qwp = [[None, None], [None, None], [None, None], [None, None]]
-        counts = [7265, 3854, 1549, 12073]
+        counts = [8137, 2319, 5542, 6989]
         append_to_output(output, names, counts)
 
         #DH
         names = ["DH", "DV", "AH", "AV",]
         angles_hwp = [[17.5, None], [17.5, 43], [62.5, None], [62.5, 43]]
         angels_qwp = [[None, None], [None, None], [None, None], [None, None]]
-        counts = [5189, 8856, 6786, 5098]
+        counts = [4193, 9699, 4863, 3619]
         append_to_output(output, names, counts)
 
         #DD
         names = ["DD", "DA", "AD", "AA",]
         angles_hwp = [[17.5, 20], [17.5, 65.5], [62.5, 20], [62.5, 65.5]]
         angels_qwp = [[None, None], [None, None], [None, None], [None, None]]
-        counts = [1661, 10690,  9403, 1936]
+        counts = [1759, 9907, 9019, 1029]
         append_to_output(output, names, counts)
 
         #HR
         names = ["HR", "HL", "VR", "VL",]
         angles_hwp = [[None, None], [None, None], [40, None], [40, None]]
         angels_qwp = [[None, 42.5], [None, 132.5], [None, 42.5], [None, 132.5]]
-        counts = [5504, 6509, 5906, 6209]
+        counts = [5814, 7152,  5725, 3655]
         append_to_output(output, names, counts)
 
         #RH
         names = ["RH", "RV", "LH", "LV",]
         angles_hwp = [[None, None], [None, 40], [None, None], [None, 40]]
-        angels_qwp = [[42.5, None], [42.5, None], [132.5, None], [132.5, None]]
-        counts = [5115, 7027, 5553, 6681]
+        angels_qwp = [[42.5, None], [42.5, None], [130, None], [132.5, None]]
+        counts = [4655, 6766, 3609, 7043]
         append_to_output(output, names, counts)
 
         #DR
         names = ["DR", "DL", "AR", "AL",]
         angles_hwp = [[17.5, None], [17.5, None], [62.5, None], [62.5, None]]
         angels_qwp = [[None, 42.5], [None, 132.5], [None, 42.5], [None, 132.5]]
-        counts = [4208, 9450, 7450, 2397]
+        counts = [6707, 6019, 4150, 4091]
         append_to_output(output, names, counts)
 
         #RD
         names = ["RD", "RA", "LD", "LA",]
         angles_hwp = [[None, 20], [None, 65.5], [None, 20], [None, 65.5]]
         angels_qwp = [[40, None], [40, None], [130, None], [130, None]]
-        counts = [9305, 3261, 3679, 7675]
+        counts = [5617, 5461, 5989, 4201]
         append_to_output(output, names, counts)
 
         #RR
         names = ["RR", "RL", "LR", "LL",]
         angles_hwp = [[None, None], [None, None], [None, None], [None, None]]
         angels_qwp = [[40, 42.5], [40, 132.5], [130, 42.5], [130, 132.5]]
-        counts = [1446, 9573, 10458, 1745]
+        counts = [787, 9833, 9579, 878]
         append_to_output(output, names, counts)
 
-load_data(1, output)
+load_data(2, output)
 
 tomo_input = np.array(output)
 intensity = np.ones(len(output))
 t = qKLib.Tomography()
 rho, intens, fval = t.state_tomography(tomo_input, intensity)
 
+# calculate fidelity
+psip = np.array([0, 1, 1, 0])
+psim = np.array([0, 1, -1, 0])
+phip = np.array([1, 0, 0, 1])
+phim = np.array([1, 0, 0, -1])
+
+for state in [psip, psim, phip, phim]:
+    print("Fidelity with state {}: {}".format(state, qKLib.fidelity(rho, state)))
 
 fig = plt.figure()
 ax1 = fig.add_subplot(121, projection='3d')
